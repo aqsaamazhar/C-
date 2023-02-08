136cd45118732464367d60a187f4a101cba84131
Aqsa mazhar <124263826+aqsaamazhar@users.noreply.github.com>
2023-02-08 10:06:38 +0500

    Add files via upload

---
+++ if-else-if.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int time=20;
+    if(time<18)
+    {
+    cout<<"Good Morning";
+    }
+     else if (time>18)
+    {
+    cout<<" Good Noon";
+    }
+    else if (time=18)
+    { 
+    cout<<"Good Evening";
+    }
+    else
+    {
+     cout<<"Good Night";
+    }
+     return 0;
+ }  
+    
