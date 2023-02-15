26187c1f3d1506b65ed7ad146eaa6d7fea4688c3
Aqsa mazhar <124263826+aqsaamazhar@users.noreply.github.com>
2023-02-15 09:02:32 +0500

    Add files via upload

---
+++ program 4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<ctype.h>
+using namespace std;
+int main()
+{
+cout<<"There was a man is peru\n";
+cout<<"who dreamed the he was acting his shoe";
+cout<<"He work in light";
+cout<<"it was a lucky night";
+cout<<"the homage went to the shoe to cnat with it ";
+return 0;
+}
